56b8a52d47b544a26363cfbf5ea1b1efcc3759f4
rayan <rayanraihani9@gmail.com>
2023-07-20 09:24:27 +0200

    change

--- recap.php
+++ recap.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     session_start();
 ?>
 <!DOCTYPE html>
@@ -6,15 +6,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Récapitulatif des produits</title>
+
+    <title> Récapitualif des produits </title>
 </head>
 <body>
-    <?php
-        if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
-            echo "<p>Aucun product en session...</p>";
+    <?php 
+        if(!isset($_SESSION["products"])|| empty($_SESSION["products"])){
+            echo "<p> Aucun produit en session... </p>";
         }
-        else{
-            echo "<tables>",
+        else {
+            echo "<table>",
                     "<thead>",
                         "<tr>",
                             "<th>#</th>",
@@ -22,26 +23,27 @@
                             "<th>Prix</th>",
                             "<th>Quantité</th>",
                             "<th>Total</th>",
-                    
-                        "<tr>",
+                        "</tr>",
                     "</thead>",
-                "<tbody>";
-
-            foreach($_SESSION['products'] as $index => $product){
+                    "<tbody>";
+                    $totalGeneral=0;
+            foreach($_SESSION["products"]as $index=> $product){
                 echo "<tr>",
                         "<td>".$index."</td>",
-                        "<td>".$product['name']."</td>",
-                        "<td>".number_format($product['price'],2, ",", "&nbsp;")."</td>",
-                        "<td>".$product['qtt']."</td>",
-                        "<td>".number_format($product['total'],2, ",", "&nbsp;")."</td>",
+                        "<td>".$product["name"]."</td>",
+                        "<td>".number_format($product["price"],2,",","&nbsp;")."&nbsp;€</td>",
+                        "<td>".$product["qtt"]."</td>",
+                        "<td>".number_format($product["total"],2,",","&nbsp;")."&nbsp;€</td>",
                     "</tr>";
+                    $totalGeneral +=$product ["total"];
             }
-            echo "</tbody>",
-                "</table>";
-            
+            echo "<tr>",
+                    "<td colspan=4>Total général : </td>",
+                    "<td><strong>".number_format($totalGeneral,2,",","&nbsp;")."&nbsp;€</strong></td>",
+                    "</tr>",
+            "</tbody>",
+            "</tables>";
         }
-    
-    
-    ?>
+        ?>
 </body>
 </html>
