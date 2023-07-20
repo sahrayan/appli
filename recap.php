ece8b450d42214df0e51be8d636604212882db73
rayan <rayanraihani9@gmail.com>
2023-07-20 09:51:51 +0200

    ajout du css

--- recap.php
+++ recap.php
@@ -6,44 +6,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css"> <!-- Fichier CSS personnalisé -->
 
-    <title> Récapitualif des produits </title>
+    <title>Récapitulatif des produits</title>
 </head>
 <body>
-    <?php 
-        if(!isset($_SESSION["products"])|| empty($_SESSION["products"])){
-            echo "<p> Aucun produit en session... </p>";
-        }
-        else {
-            echo "<table>",
-                    "<thead>",
-                        "<tr>",
-                            "<th>#</th>",
-                            "<th>Nom</th>",
-                            "<th>Prix</th>",
-                            "<th>Quantité</th>",
-                            "<th>Total</th>",
-                        "</tr>",
-                    "</thead>",
-                    "<tbody>";
-                    $totalGeneral=0;
-            foreach($_SESSION["products"]as $index=> $product){
-                echo "<tr>",
-                        "<td>".$index."</td>",
-                        "<td>".$product["name"]."</td>",
-                        "<td>".number_format($product["price"],2,",","&nbsp;")."&nbsp;€</td>",
-                        "<td>".$product["qtt"]."</td>",
-                        "<td>".number_format($product["total"],2,",","&nbsp;")."&nbsp;€</td>",
-                    "</tr>";
-                    $totalGeneral +=$product ["total"];
+    <div class="container mt-5">
+        <?php 
+            if(!isset($_SESSION["products"]) || empty($_SESSION["products"])) {
+                echo "<p>Aucun produit en session...</p>";
+            } else {
+                echo '<table class="table">',
+                        '<thead class="thead-light">',
+                            '<tr>',
+                                '<th>#</th>',
+                                '<th>Nom</th>',
+                                '<th>Prix</th>',
+                                '<th>Quantité</th>',
+                                '<th>Total</th>',
+                            '</tr>',
+                        '</thead>',
+                        '<tbody>';
+                $totalGeneral = 0;
+                foreach($_SESSION["products"] as $index => $product) {
+                    echo '<tr>',
+                            '<td>'.$index.'</td>',
+                            '<td>'.$product["name"].'</td>',
+                            '<td>'.number_format($product["price"], 2, ",", "&nbsp;").'€</td>',
+                            '<td>'.$product["qtt"].'</td>',
+                            '<td>'.number_format($product["total"], 2, ",", "&nbsp;").'€</td>',
+                        '</tr>';
+                    $totalGeneral += $product["total"];
+                }
+                echo '<tr>',
+                        '<td colspan="4" class="text-right"><strong>Total général : </strong></td>',
+                        '<td><strong>'.number_format($totalGeneral, 2, ",", "&nbsp;").'€</strong></td>',
+                    '</tr>',
+                '</tbody>',
+                '</table>';
             }
-            echo "<tr>",
-                    "<td colspan=4>Total général : </td>",
-                    "<td><strong>".number_format($totalGeneral,2,",","&nbsp;")."&nbsp;€</strong></td>",
-                    "</tr>",
-            "</tbody>",
-            "</tables>";
-        }
         ?>
+    </div>
 </body>
 </html>
