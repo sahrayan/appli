9e9dec1bc598fea0b0660763bde2d136fd7c1cd8
rayan <rayanraihani9@gmail.com>
2023-07-20 09:27:28 +0200

    propre

--- traitement.php
+++ traitement.php
@@ -1,20 +1,23 @@
 <?php
-session_start();
+    session_start(); 
 
-if (isset($_POST['submit'])){
-    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING);
-    $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-    $qtt = filter_input(INPUT_POST, "qtt",FILTER_VALIDATE_INT);
-    if ($name && $price && $qtt){
-        $product = [
-            "name" => $name,
-            "price" => $price,
-            "qtt" => $qtt,
-            "total" => $price*$qtt
-        ];
-        $_SESSION['product'] = $product;
-    }
-}
+    if(isset($_POST['submit'])){ 
 
-header("Location:index.php")
+        $name = filter_input(INPUT_POST,"name",FILTER_SANITIZE_FULL_SPECIAL_CHARS); 
+        $price= filter_input(INPUT_POST,"price",FILTER_VALIDATE_FLOAT,FILTER_FLAG_ALLOW_FRACTION); 
+        $qtt= filter_input(INPUT_POST,"qtt",FILTER_VALIDATE_INT); 
+        if($name && $price && $qtt){
+
+            $product = [
+                "name" => $name,
+                "price" => $price,
+                "qtt" => $qtt,
+                "total" => $price*$qtt
+            ];
+
+            $_SESSION["product"][]=$product;
+        }
+    } 
+
+    header("Location:index.php");
 ?>
