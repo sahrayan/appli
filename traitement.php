57d9d8b75087e487da39bbed8db4579e62e177b9
rayan <rayanraihani9@gmail.com>
2023-07-20 09:52:21 +0200

    ajt  du calcul des prix

--- traitement.php
+++ traitement.php
@@ -1,23 +1,27 @@
 <?php
-    session_start(); 
+session_start(); 
 
-    if(isset($_POST['submit'])){ 
+if (isset($_POST['submit'])) { 
 
-        $name = filter_input(INPUT_POST,"name",FILTER_SANITIZE_FULL_SPECIAL_CHARS); 
-        $price= filter_input(INPUT_POST,"price",FILTER_VALIDATE_FLOAT,FILTER_FLAG_ALLOW_FRACTION); 
-        $qtt= filter_input(INPUT_POST,"qtt",FILTER_VALIDATE_INT); 
-        if($name && $price && $qtt){
+    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS); 
+    $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION); 
+    $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT); 
+    if ($name && $price !== false && $qtt !== false) {
 
-            $product = [
-                "name" => $name,
-                "price" => $price,
-                "qtt" => $qtt,
-                "total" => $price*$qtt
-            ];
+        $product = [
+            "name" => $name,
+            "price" => $price,
+            "qtt" => $qtt,
+            "total" => $price * $qtt
+        ];
 
-            $_SESSION["product"][]=$product;
+        if (!isset($_SESSION["products"]) || empty($_SESSION["products"])) {
+            $_SESSION["products"] = array(); // Initialise la session si elle est vide
         }
-    } 
 
-    header("Location:index.php");
+        $_SESSION["products"][] = $product; // Ajoute le produit à la session
+    }
+} 
+
+header("Location: index.php"); // Redirige l'utilisateur vers la page index.php
 ?>
